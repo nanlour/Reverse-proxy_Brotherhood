42d132fb549416e8fc6323322aa66a14b30d7ce4
wr133 <tjuwrr@gmail.com>
2023-03-08 12:19:34 +1100

    litter change of server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from queue import Queue
 from threading import Thread
 
 LOCALHOST = 'localhost'
-VPS = ''
+VPS = '192.168.217.93'
 
 
 def echo_handler_s(client_sock, queue_s, queue_c):
@@ -11,11 +11,13 @@ def echo_handler_s(client_sock, queue_s, queue_c):
         msg_c = client_sock.recv(8192)
         print(1, msg_c)
         queue_c.put(msg_c)
+        print(1)
 
     def c_s(queue_s, client_sock):
         msg_s = queue_s.get()
         print(2, msg_s)
         client_sock.sendall(msg_s)
+        print(2)
 
     t1 = Thread(target=c_s, args=(queue_s, client_sock))
     t2 = Thread(target=s_c, args=(queue_c, client_sock))
@@ -29,11 +31,13 @@ def echo_handler_c(client_sock, queue_s, queue_c):
         msg_s = client_sock.recv(8192)
         print(3, msg_s)
         queue_s.put(msg_s)
+        print(3)
 
     def s_c(queue_c, client_sock):
         msg_c = queue_c.get()
         print(4, msg_c)
         client_sock.sendall(msg_c)
+        print(4)
 
     t1 = Thread(target=c_s, args=(queue_s, client_sock))
     t2 = Thread(target=s_c, args=(queue_c, client_sock))
@@ -52,6 +56,7 @@ def echo_server(address, queue_s, queue_c, s_c):
             else:
                 echo_handler_c(client_sock, queue_s, queue_c)
         except:
+            print(9)
             pass
 
 
